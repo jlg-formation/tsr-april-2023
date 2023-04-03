bfdfeac5332d180ad8513efa4f8a1dfd2676639e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-03 14:44:51 +0200

    dev: added typescript to the project

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,22 +10,29 @@ const samples = 10;
 
 const svgns = "http://www.w3.org/2000/svg";
 const gSampleElement = document.querySelector("svg g.samples");
+if (gSampleElement === null) {
+  throw new Error("Cannot find selector svg g.samples");
+}
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples;
 
   const cx = cx0 + r0 * Math.cos(angle);
   const cy = cy0 + r0 * Math.sin(angle);
+  const r = 1;
 
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", cx);
-  circle.setAttributeNS(null, "cy", cy);
-  circle.setAttributeNS(null, "r", 1);
+  circle.setAttributeNS(null, "cx", cx + "");
+  circle.setAttributeNS(null, "cy", cy + "");
+  circle.setAttributeNS(null, "r", r + "");
 
   gSampleElement.appendChild(circle);
 }
 
 // make the lines
 const gLineElement = document.querySelector("svg g.lines");
+if (gLineElement === null) {
+  throw new Error("Cannot find selector svg g.lines");
+}
 for (let i = 0; i < samples; i++) {
   const angle1 = (i * 2 * Math.PI) / samples;
   const angle2 = angle1 * multiplicationFactor;
@@ -36,10 +43,10 @@ for (let i = 0; i < samples; i++) {
   const y2 = cy0 + r0 * Math.sin(angle2);
 
   const line = document.createElementNS(svgns, "line");
-  line.setAttributeNS(null, "x1", x1);
-  line.setAttributeNS(null, "y1", y1);
-  line.setAttributeNS(null, "x2", x2);
-  line.setAttributeNS(null, "y2", y2);
+  line.setAttributeNS(null, "x1", x1 + "");
+  line.setAttributeNS(null, "y1", y1 + "");
+  line.setAttributeNS(null, "x2", x2 + "");
+  line.setAttributeNS(null, "y2", y2 + "");
 
   gLineElement.appendChild(line);
 }
