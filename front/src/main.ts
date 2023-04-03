13e9706dc973c5b2ab027923bbc0bdfa2830989e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-03 15:32:37 +0200

    dev: added vite

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,52 +1,24 @@
-import {
-  cx0,
-  cy0,
-  multiplicationFactor,
-  r0,
-  samples,
-  svgns,
-} from "./constants.js";
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter'
 
-console.log("hello");
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-const gSampleElement = document.querySelector("svg g.samples");
-if (gSampleElement === null) {
-  throw new Error("Cannot find selector svg g.samples");
-}
-for (let i = 0; i < samples; i++) {
-  const angle = (i * 2 * Math.PI) / samples;
-
-  const cx = cx0 + r0 * Math.cos(angle);
-  const cy = cy0 + r0 * Math.sin(angle);
-  const r = 1;
-
-  const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", cx + "");
-  circle.setAttributeNS(null, "cy", cy + "");
-  circle.setAttributeNS(null, "r", r + "");
-
-  gSampleElement.appendChild(circle);
-}
-
-// make the lines
-const gLineElement = document.querySelector("svg g.lines");
-if (gLineElement === null) {
-  throw new Error("Cannot find selector svg g.lines");
-}
-for (let i = 0; i < samples; i++) {
-  const angle1 = (i * 2 * Math.PI) / samples;
-  const angle2 = angle1 * multiplicationFactor;
-
-  const x1 = cx0 + r0 * Math.cos(angle1);
-  const y1 = cy0 + r0 * Math.sin(angle1);
-  const x2 = cx0 + r0 * Math.cos(angle2);
-  const y2 = cy0 + r0 * Math.sin(angle2);
-
-  const line = document.createElementNS(svgns, "line");
-  line.setAttributeNS(null, "x1", x1 + "");
-  line.setAttributeNS(null, "y1", y1 + "");
-  line.setAttributeNS(null, "x2", x2 + "");
-  line.setAttributeNS(null, "y2", y2 + "");
-
-  gLineElement.appendChild(line);
-}
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
