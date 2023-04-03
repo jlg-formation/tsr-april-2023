313f5375e4f76ff55af764796d41af75d1e160aa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-03 11:10:30 +0200

    refactor: replace var with const and let
    prefer const instead of let

--- front/main.js
+++ front/main.js
@@ -1,20 +1,20 @@
 console.log("hello");
 
-var cx0 = 50;
-var cy0 = 50;
-var r0 = 45;
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
 
-var samples = 10;
+const samples = 10;
 
-var svgns = "http://www.w3.org/2000/svg";
-var svgElement = document.querySelector("svg g.samples");
-for (var i = 0; i < samples; i++) {
-  var angle = (i * 2 * Math.PI) / samples;
+const svgns = "http://www.w3.org/2000/svg";
+const svgElement = document.querySelector("svg g.samples");
+for (let i = 0; i < samples; i++) {
+  const angle = (i * 2 * Math.PI) / samples;
 
-  var cx = cx0 + r0 * Math.cos(angle);
-  var cy = cy0 + r0 * Math.sin(angle);
+  const cx = cx0 + r0 * Math.cos(angle);
+  const cy = cy0 + r0 * Math.sin(angle);
 
-  var circle = document.createElementNS(svgns, "circle");
+  const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", cx);
   circle.setAttributeNS(null, "cy", cy);
   circle.setAttributeNS(null, "r", 1);
